4d603543b4de05bd4681f49bbe628f8a848389e9
Andrew Zicler <68751913+douile17@users.noreply.github.com>
2024-04-14 14:35:28 +0200

    Update PastaBoxInterface.py

--- PastaBoxInterface.py
+++ PastaBoxInterface.py
@@ -3,7 +3,7 @@ from tkinter import filedialog, scrolledtext, messagebox
 import pandas as pd
 import serial
 from serial.tools.list_ports import comports
-from threading import Thread
+from threading import Thread, Event
 from time import sleep, strftime, localtime
 
 class SerialController:
@@ -11,51 +11,49 @@ class SerialController:
         self.port = port
         self.console = console
         self.app = app
-        self.ser = serial.Serial(baudrate=9600, timeout=1)
-        self.running = False
-        self.paused = False
+        self.ser = None
+        self.running_event = Event()
+        self.paused_event = Event()
         self.current_index = 0
 
     def start(self, filename):
-        self.ser.port = self.port
-        self.ser.open()
-        self.running = True
-        self.paused = False
-        self.filename = filename
-        self.app.update_console("Sequence started:")
-        self.thread = Thread(target=self.send_data)
+        self.running_event.set()
+        self.paused_event.clear()
+        self.ser = serial.Serial(self.port, baudrate=9600, timeout=1)
+        self.app.update_console("Script started.")
+        self.thread = Thread(target=self.send_data, args=(filename,))
         self.thread.start()
 
     def stop(self):
-        self.running = False
-        if self.ser.is_open:
+        self.running_event.clear()
+        self.paused_event.clear()
+        if self.ser and self.ser.is_open:
             self.ser.write(b'D')
             self.ser.close()
-        self.app.update_console("Sequence stopped.")
+        self.app.update_console("Script stopped.")
         self.app.reset_buttons()
+        self.current_index = 0
 
     def pause(self):
-        self.paused = True
-        self.app.update_console("Sequence paused.")
+        self.paused_event.set()
+        self.app.update_console("Script paused.")
 
     def resume(self):
-        self.paused = False
-        self.app.update_console("Sequence resumed.")
+        self.paused_event.clear()
+        self.app.update_console("Script resumed.")
 
-    def send_data(self):
-        data = self.load_csv(self.filename)
+    def send_data(self, filename):
+        data = self.load_csv(filename)
         if data is None:
             self.stop()
             return
 
         for index, row in data.iterrows():
-            self.current_index = index
-            if not self.running:
+            if not self.running_event.is_set():
                 return
 
-            if self.paused:
-                while self.paused:
-                    sleep(0.1)
+            if self.paused_event.is_set():
+                self.paused_event.wait()
 
             time_value = row['Time (min)']
             output_value = row['Output']
@@ -65,7 +63,7 @@ class SerialController:
             self.console.insert(tk.END, msg + '\n')
             self.console.see(tk.END)
 
-            if self.ser.is_open:
+            if self.ser and self.ser.is_open:
                 self.ser.write(output_value.encode())
 
             if index < len(data) - 1:
@@ -73,7 +71,6 @@ class SerialController:
                 time_difference = (next_time_value - time_value) * 60
                 sleep(time_difference)
         
-        # Si la lecture du fichier CSV est terminée, arrêter le script et réinitialiser les boutons
         self.stop()
 
     def load_csv(self, filename):
@@ -88,18 +85,11 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title("Pasta Box")
-        self.root.geometry("600x400")  # Définit la taille de la fenêtre
+        self.root.geometry("600x400")
 
-        # Définir la couleur de fond
-        self.bg_color = "#f0f0f0"  # Couleur de fond
-        self.button_bg_color = "#FFFFFF"  # Couleur de fond des boutons
-        self.button_disabled_color = "#D3D3D3"  # Couleur de fond désactivée des boutons
-        self.button_border_color = "#808080"  # Couleur du contour des boutons
-        root.resizable(False, False)  # Verrouille la fenêtre pour empêcher le redimensionnement
-        self.root.configure(bg=self.bg_color)  # Appliquer la couleur de fond à la fenêtre
-
-        # Définir l'icône
-        self.root.iconbitmap('favicon.ico')
+        self.bg_color = "#f0f0f0"
+        self.button_bg_color = "#FFFFFF"
+        self.root.configure(bg=self.bg_color)
 
         self.port_frame = tk.Frame(root, bg=self.bg_color)
         self.port_frame.grid(row=0, column=0, padx=10, pady=5, sticky="w")
@@ -107,22 +97,23 @@ class App:
         self.port_label = tk.Label(self.port_frame, text="Select COM port:", bg=self.bg_color)
         self.port_label.grid(row=0, column=0, padx=(0, 5))
 
-        self.ports = [port_info.device for port_info in comports()]
-        self.port_dropdown = None
-        self.ports_available_label = tk.Label(self.port_frame, text="", bg=self.bg_color)  # Initialisation de l'attribut
-        self.ports_available_label.grid(row=1, columnspan=2)  # Positionnement de l'annotation
+        self.ports = self.get_serial_ports()
+        self.port_var = tk.StringVar()
+        self.port_dropdown = tk.OptionMenu(self.port_frame, self.port_var, *self.ports)
+        self.port_dropdown.config(bg=self.button_bg_color, bd=1, relief=tk.FLAT, width=30)
 
-        self.refresh_ports()
+        for option in self.port_dropdown.children.values():
+            option.config(bg="#FFFFFF", relief=tk.FLAT)
+
+        self.port_dropdown.grid(row=0, column=1)
 
         self.load_button = tk.Button(root, text="Load CSV", command=self.load_csv, bg=self.button_bg_color, bd=1, relief=tk.FLAT)
         self.load_button.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
-        # Création du bouton "Start" désactivé et grisé
-        self.start_stop_button = tk.Button(root, text="Start", command=self.start_stop, width=10, highlightbackground="green", bg=self.button_disabled_color, activebackground=self.button_bg_color, relief=tk.FLAT, borderwidth=1, state=tk.DISABLED, disabledforeground="#A9A9A9")
+        self.start_stop_button = tk.Button(root, text="Start", command=self.start_stop, width=10, highlightbackground="green", bg=self.button_bg_color, activebackground=self.button_bg_color, relief=tk.FLAT, borderwidth=1, state=tk.DISABLED)
         self.start_stop_button.grid(row=0, column=2, padx=10, pady=5, sticky="e")
 
-        # Création du bouton "Pause" désactivé et grisé
-        self.pause_button = tk.Button(root, text="Pause", command=self.pause_resume, width=10, highlightbackground="red", bg=self.button_disabled_color, activebackground=self.button_bg_color, relief=tk.FLAT, borderwidth=1, state=tk.DISABLED, disabledforeground="#A9A9A9")
+        self.pause_button = tk.Button(root, text="Pause", command=self.pause_resume, width=10, highlightbackground="red", bg=self.button_bg_color, activebackground=self.button_bg_color, relief=tk.FLAT, borderwidth=1, state=tk.DISABLED)
         self.pause_button.grid(row=1, column=2, padx=10, pady=5, sticky="e")
 
         self.console_label = tk.Label(root, text="Console:")
@@ -138,44 +129,24 @@ class App:
         self.refresh_button.place(x=340, y=8)
 
         self.serial_controller = None
-        self.paused = False
 
         root.grid_rowconfigure(3, weight=1)
         root.grid_columnconfigure(0, weight=1)
 
-        # Gestion de la fermeture de la fenêtre
         root.protocol("WM_DELETE_WINDOW", self.on_close)
 
+    def get_serial_ports(self):
+        ports = []
+        for port_info in comports():
+            ports.append(f"{port_info.device} - {port_info.description}")
+        return ports
+
     def refresh_ports(self):
-        self.ports = [port_info.device for port_info in comports()]
-
-        # Supprimer l'ancien menu déroulant s'il existe
-        if self.port_dropdown:
-            self.port_dropdown.destroy()
-
-        if self.ports:
-            # Créer un nouveau menu déroulant seulement s'il y a des ports disponibles
-            formatted_ports = [f"{port_info.device} - {port_info.description}" for port_info in comports()]
-            self.port_var = tk.StringVar()
-
-            # Option par défaut avec l'annotation
-            default_option = "COM ports available ->"
-            self.port_var.set(default_option)
-            self.port_dropdown = tk.OptionMenu(self.port_frame, self.port_var, *formatted_ports)
-
-            self.port_dropdown.config(bg=self.button_bg_color, bd=1, relief=tk.FLAT, width=30)
-            for option in self.port_dropdown.children.values():
-                option.config(bg="#FFFFFF", relief=tk.FLAT)
-
-            self.port_dropdown.grid(row=0, column=1)
-
-            # Afficher l'annotation des ports disponibles
-            self.ports_available_label.config(text="", fg=self.bg_color)
-        else:
-            # Affichez un message si aucun port n'est disponible
-            self.ports_available_label.config(text="", fg=self.bg_color)
-
-            tk.Label(self.port_frame, text="No serial ports available", bg=self.bg_color).grid(row=0, column=1)
+        self.ports = self.get_serial_ports()
+        menu = self.port_dropdown["menu"]
+        menu.delete(0, "end")
+        for port in self.ports:
+            menu.add_command(label=port, command=lambda value=port: self.port_var.set(value))
 
     def load_csv(self):
         filename = filedialog.askopenfilename(title="Select CSV File", filetypes=[("CSV files", "*.csv")])
@@ -186,29 +157,12 @@ class App:
             self.update_console("CSV file loaded successfully: " + filename)
 
     def start_stop(self):
-        if not self.ports:
-            self.update_console("No serial ports available")
-            return
-
         if not self.port_var.get():
             self.update_console("Please select a COM port.")
             return
 
-        selected_port = self.port_var.get().split(" - ")[0]
-
-        # Vérifier si le port sélectionné est dans la liste des ports disponibles
-        if selected_port not in self.ports:
-            self.update_console("Selected port is not available.")
-            return
-
-        if self.serial_controller is None:
-            self.serial_controller = SerialController(selected_port, self.console, self)
-
-        if self.start_stop_button.cget("text") == "Start":
-            if not hasattr(self, 'filename') or not self.filename:
-                self.update_console("Please load a CSV file.")
-                return
-
+        if self.serial_controller is None or not self.serial_controller.running_event.is_set():
+            self.serial_controller = SerialController(self.port_var.get().split(" - ")[0], self.console, self)
             self.serial_controller.start(self.filename)
             self.start_stop_button.config(text="Stop", bg="red")
             self.pause_button.config(state=tk.NORMAL)
@@ -219,22 +173,20 @@ class App:
 
     def pause_resume(self):
         if self.serial_controller is not None:
-            if self.paused:
-                self.paused = False
-                self.pause_button.config(text="Pause")
+            if self.serial_controller.paused_event.is_set():
                 self.serial_controller.resume()
+                self.pause_button.config(text="Pause")
             else:
-                self.paused = True
-                self.pause_button.config(text="Resume")
                 self.serial_controller.pause()
+                self.pause_button.config(text="Resume")
 
     def clear_console(self):
         self.console.delete(1.0, tk.END)
 
     def update_console(self, message):
+        num_dashes = 69
         if "| Csv time:" not in message and "| Current time:" not in message:
-            num_dashes = 69  # Nombre de tirets souhaité
-            self.console.insert(tk.END, '-' * num_dashes + '\n')  # Ajout de la ligne séparatrice
+            self.console.insert(tk.END, '-' * num_dashes + '\n')
         self.console.insert(tk.END, message + '\n')
         self.console.see(tk.END)
 
@@ -243,7 +195,7 @@ class App:
         self.pause_button.config(state=tk.DISABLED)
 
     def on_close(self):
-        if self.serial_controller is not None:
+        if self.serial_controller is not None and self.serial_controller.running_event.is_set():
             if messagebox.askokcancel("Quit", "Do you want to quit?"):
                 self.serial_controller.stop()
                 self.root.destroy()
