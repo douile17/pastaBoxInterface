a7118cdd16aa0da1d65fdedd07e84390aba40828
Andrew Zicler <68751913+douile17@users.noreply.github.com>
2024-04-16 21:15:02 +0200

    Update PastaBoxInterface.py

--- PastaBoxInterface.py
+++ PastaBoxInterface.py
@@ -1,230 +1,191 @@
-import tkinter as tk
-from tkinter import filedialog, scrolledtext, messagebox
 import pandas as pd
+from tkinter import ttk
 import serial
-from serial.tools.list_ports import comports
+import serial.tools.list_ports
+from tkinter import *
+from tkinter import filedialog, messagebox
 from threading import Thread, Event
-from time import sleep, strftime, localtime
+from time import sleep
+from datetime import datetime
 
-class SerialController:
-    def __init__(self, port, console, app):
-        self.port = port
-        self.console = console
-        self.app = app
-        self.ser = None
-        self.running_event = Event()
-        self.paused_event = Event()
-        self.current_index = 0
+# Variables globales pour suivre l'état de la connexion au port COM, du chargement du fichier CSV et de l'envoi de données
+connected = False
+csv_loaded = False
+sending = False
+send_thread = None
 
-    def start(self, filename):
-        self.running_event.set()
-        self.paused_event.clear()
-        self.ser = serial.Serial(self.port, baudrate=9600, timeout=1)
-        self.app.update_console("Script started.")
-        self.thread = Thread(target=self.send_data, args=(filename,))
-        self.thread.start()
+# Fonction pour rechercher les ports COM disponibles
+def find_serial_ports():
+    ports = serial.tools.list_ports.comports()
+    available_ports = []
+    for port in ports:
+        available_ports.append((port.device, port.description))
+    return available_ports
 
-    def stop(self):
-        self.running_event.clear()
-        self.paused_event.clear()
-        if self.ser and self.ser.is_open:
-            self.ser.write(b'D')
-            self.ser.close()
-        self.app.update_console("Script stopped.")
-        self.app.reset_buttons()
-        self.current_index = 0
+# Fonction pour se connecter au port série
+def connect_serial():
+    global ser, connected, send_button
+    try:
+        serial_port = port_combo.get().split()[0]  # Récupérer le port sélectionné depuis la ComboBox
+        ser = serial.Serial(serial_port, baudrate=9600, timeout=1)
+        connected = True
+        connect_button.config(text="Disconnect", command=disconnect_serial)
+        add_to_console(f"Connected to {serial_port}")
+        if csv_loaded:  # Si le fichier CSV est chargé, activer le bouton "Start"
+            root.after(1000, lambda: send_button.config(state="normal", bg="green", fg="white", text="Start Sequence"))
+    except serial.SerialException as e:
+        messagebox.showerror("Error", str(e))
 
-    def pause(self):
-        self.paused_event.set()
-        self.app.update_console("Script paused.")
+# Fonction pour se déconnecter du port série
+def disconnect_serial():
+    global ser, connected, sending, send_button
+    if connected:
+        try:
+            ser.close()
+            connected = False
+            connect_button.config(text="Connect", command=connect_serial)
+            send_button.config(state="disabled", bg="gray", fg="black", text="Start Sequence")
+            add_to_console("Disconnected from serial port")
+            if sending:  # Si une séquence est en cours, arrêter l'envoi
+                stop_data()
+        except serial.SerialException as e:
+            messagebox.showerror("Error", str(e))
+    else:
+        print("Already disconnected")
 
-    def resume(self):
-        self.paused_event.clear()
-        self.app.update_console("Script resumed.")
+# Fonction pour charger un fichier CSV
+def load_csv():
+    global data, csv_loaded
+    file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+    if file_path:
+        try:
+            data = pd.read_csv(file_path)
+            csv_loaded = True
+            add_to_console(f"CSV file loaded: {file_path}")
+            print(f"CSV file loaded: {file_path}")
+            if connected:  # Si le port série est connecté, activer le bouton "Start"
+                root.after(1000, lambda: send_button.config(state="normal", bg="green", fg="white", text="Start Sequence"))
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
 
-    def send_data(self, filename):
-        data = self.load_csv(filename)
-        if data is None:
-            self.stop()
-            return
-
-        for index, row in data.iterrows():
-            if not self.running_event.is_set():
-                return
-
-            if self.paused_event.is_set():
-                self.paused_event.wait()
-
-            time_value = row['Time (min)']
-            output_value = row['Output']
-            current_time = strftime("%H:%M:%S", localtime())
-            msg = f"{output_value} | Csv time: {time_value} | Current time: {current_time}"
-            print(msg)
-            self.console.insert(tk.END, msg + '\n')
-            self.console.see(tk.END)
-
-            if self.ser and self.ser.is_open:
-                self.ser.write(output_value.encode())
-
-            if index < len(data) - 1:
-                next_time_value = data.at[index + 1, 'Time (min)']
-                time_difference = (next_time_value - time_value) * 60
-                sleep(time_difference)
-        
-        self.stop()
-
-    def load_csv(self, filename):
-        if filename:
-            try:
-                return pd.read_csv(filename)
-            except Exception as e:
-                print("Error loading CSV file:", e)
-        return None
-
-class App:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Pasta Box")
-        root.iconbitmap('favicon.ico')
-        # Calculez les dimensions de l'écran
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-
-        # Calculez les coordonnées x et y pour centrer la fenêtre
-        x = (screen_width - 600) / 2
-        y = (screen_height - 400) / 2
-
-        # Placez la fenêtre au centre de l'écran
-        self.root.geometry("+%d+%d" % (x, y))
-
-        self.bg_color = "#f0f0f0"
-        self.button_bg_color = "#FFFFFF"
-        self.root.configure(bg=self.bg_color)
-
-        self.port_frame = tk.Frame(root, bg=self.bg_color)
-        self.port_frame.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-
-        self.port_label = tk.Label(self.port_frame, text="Select COM port:", bg=self.bg_color)
-        self.port_label.grid(row=0, column=0, padx=(0, 5))
-
-        self.ports = self.get_serial_ports()
-        self.port_var = tk.StringVar()
-        self.port_var.set("Select COM port ->")  # Définit la valeur par défaut du menu déroulant
-        self.port_dropdown = tk.OptionMenu(self.port_frame, self.port_var, *self.ports)
-        self.port_dropdown.config(bg=self.button_bg_color, bd=1, relief=tk.FLAT, width=30,activebackground="#0078D7", activeforeground="white")
-
-        for option in self.port_dropdown.children.values():
-            option.config(bg="#FFFFFF", relief=tk.FLAT )
-
-        self.port_dropdown.grid(row=0, column=1)
-
-        # Ajoutez ces lignes pour lier les événements de survol de la souris
-        self.port_dropdown.bind("<Enter>", self.on_enter_port_dropdown)
-        self.port_dropdown.bind("<Leave>", self.on_leave_port_dropdown)
-
-        self.load_button = tk.Button(root, text="Load CSV", command=self.load_csv, bg=self.button_bg_color, bd=1, relief=tk.FLAT)
-        self.load_button.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-
-        self.start_stop_button = tk.Button(root, text="Start", command=self.start_stop, width=10, highlightbackground="green", bg="#c2c0c0", activebackground=self.button_bg_color, relief=tk.FLAT, borderwidth=1, state=tk.DISABLED)
-        self.start_stop_button.grid(row=0, column=2, padx=10, pady=5, sticky="e")
-
-        self.pause_button = tk.Button(root, text="Pause", command=self.pause_resume, width=10, highlightbackground="red", bg="#c2c0c0", activebackground=self.button_bg_color, relief=tk.FLAT, borderwidth=1, state=tk.DISABLED)
-        self.pause_button.grid(row=1, column=2, padx=10, pady=5, sticky="e")
-
-        self.console_label = tk.Label(root, text="Console:")
-        self.console_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
-
-        self.console = scrolledtext.ScrolledText(root, wrap=tk.WORD)
-        self.console.grid(row=3, column=0, columnspan=3, padx=10, pady=5, sticky="nsew")
-
-        self.clear_button = tk.Button(root, text="Clear Console", command=self.clear_console, bg=self.button_bg_color, bd=1, relief=tk.FLAT)
-        self.clear_button.grid(row=4, column=0, padx=10, pady=5, sticky="w")
-
-        self.refresh_button = tk.Button(root, text="Refresh", command=self.refresh_ports, bg=self.button_bg_color, bd=1, relief=tk.FLAT)
-        self.refresh_button.place(x=340, y=8)
-
-        self.serial_controller = None
-
-        root.grid_rowconfigure(3, weight=1)
-        root.grid_columnconfigure(0, weight=1)
-
-        root.protocol("WM_DELETE_WINDOW", self.on_close)
-
-    def get_serial_ports(self):
-        ports = []
-        for port_info in comports():
-            ports.append(f"{port_info.device} - {port_info.description}")
-        return ports
-
-    def refresh_ports(self):
-        self.ports = self.get_serial_ports()
-        menu = self.port_dropdown["menu"]
-        menu.delete(0, "end")
-        for port in self.ports:
-            menu.add_command(label=port, command=lambda value=port: self.port_var.set(value))
-
-    def load_csv(self):
-        filename = filedialog.askopenfilename(title="Select CSV File", filetypes=[("CSV files", "*.csv")])
-        if filename:
-            print("Selected CSV file:", filename)
-            self.filename = filename
-            self.start_stop_button.config(state=tk.NORMAL, text="Start", fg="white", bg="green")
-            self.update_console("CSV file loaded successfully: " + filename)
-
-    def start_stop(self):
-        if not self.port_var.get():
-            self.update_console("Please select a COM port.")
-            return
-
-        if self.serial_controller is None or not self.serial_controller.running_event.is_set():
-            self.serial_controller = SerialController(self.port_var.get().split(" - ")[0], self.console, self)
-            self.serial_controller.start(self.filename)
-            self.start_stop_button.config(text="Stop", bg="red")
-            self.pause_button.config(state=tk.NORMAL)
+# Fonction principale pour démarrer ou arrêter l'envoi de données au port série
+def toggle_send_data():
+    global sending, send_thread
+    if sending:
+        stop_data()
+    else:
+        if connected and csv_loaded:
+            sending = True
+            send_thread = Thread(target=send_data_thread)
+            send_thread.start()
+            send_button.config(text="Stop Sequence", bg="red", fg="white")
         else:
-            self.serial_controller.stop()
-            self.start_stop_button.config(text="Start", bg="green")
-            self.pause_button.config(state=tk.DISABLED)
+            messagebox.showwarning("Warning", "Please connect to a serial port and load a CSV file first.")
 
-    def pause_resume(self):
-        if self.serial_controller is not None:
-            if self.serial_controller.paused_event.is_set():
-                self.serial_controller.resume()
-                self.pause_button.config(text="Pause")
-            else:
-                self.serial_controller.pause()
-                self.pause_button.config(text="Resume")
+def stop_data():
+    global sending
+    sending = False
+    if connected:
+        ser.write(b"D")  # Envoie la lettre "D" lorsque le bouton "Stop" est pressé
+    send_button.config(text="Start Sequence", bg="green" if connected else "gray", fg="white")
 
-    def clear_console(self):
-        self.console.delete(1.0, tk.END)
+# Fonction principale pour envoyer les données au port série
+def send_data_thread():
+    global stop_event, send_button, sending
+    stop_event = Event()  # Crée un nouvel événement pour chaque cycle d'envoi de données
+    sending = True  # Définir l'état d'envoi comme True au début de l'envoi
+    for index, row in data.iterrows():
+        if not sending:  # Vérifie si l'envoi est toujours en cours
+            break  # Sort de la boucle si l'envoi est arrêté
 
-    def update_console(self, message):
-        num_dashes = 69
-        if "| Csv time:" not in message and "| Current time:" not in message:
-            self.console.insert(tk.END, '-' * num_dashes + '\n')
-        self.console.insert(tk.END, message + '\n')
-        self.console.see(tk.END)
+        current_time = datetime.now().strftime("%H:%M:%S")
+        time_value = row['Time (min)']
+        output_value = row['Output']
+        add_to_console(f"Current time: {current_time} | CSV time: {time_value} | Output: {output_value}", separate=False)
 
-    def reset_buttons(self):
-        self.start_stop_button.config(text="Start", bg="green")
-        self.pause_button.config(state=tk.DISABLED)
+        ser.write(output_value.encode())
 
-    def on_close(self):
-        if self.serial_controller is not None and self.serial_controller.running_event.is_set():
-            if messagebox.askokcancel("Quit", "Do you want to quit?"):
-                self.serial_controller.stop()
-                self.root.destroy()
-        else:
-            if messagebox.askokcancel("Quit", "Do you want to quit?"):
-                self.root.destroy()
+        next_time_value = data.at[index + 1, 'Time (min)'] if index < len(data) - 1 else None
+        if next_time_value is not None:
+            time_difference = next_time_value - time_value
+            if time_difference > 0:
+                sleep(time_difference * 60)
 
-    def on_enter_port_dropdown(self, event):
-        self.port_dropdown.config(bg="blue")  # Change la couleur de fond en bleu lorsque la souris entre
+        if not connected:  # Si la connexion au port COM est perdue, arrêter l'envoi
+            stop_data()
+            break
 
-    def on_leave_port_dropdown(self, event):
-        self.port_dropdown.config(bg=self.button_bg_color)  # Revenir à la couleur de fond d'origine lorsque la souris quitte
+    # Vérifier si l'envoi est terminé et mettre à jour le bouton en conséquence
+    if sending:
+        send_button.config(text="Start Sequence", bg="green" if connected else "gray", fg="white")
+        sending = False  # Mettre à jour l'état d'envoi comme False à la fin de l'envoi
 
-# Créez la racine et l'application
-root = tk.Tk()
-app = App(root)
+    add_to_console("Sequence finished")  # Ajouter un message à la console une fois la séquence terminée
+
+# Fonction pour rafraîchir les ports COM dans la ComboBox
+def refresh_ports():
+    ports = find_serial_ports()
+    port_combo['values'] = [f"{port} - {description}" for port, description in ports]
+    if ports:
+        port_combo.current(0)
+    else:
+        port_combo.set("No COM Ports Available")
+
+# Fonction pour ajouter un message à la console
+def add_to_console(message, separate=True):
+    console.config(state="normal")
+    console.insert(END, message + "\n")
+    if separate:
+        console.insert(END, "-" * 72 + "\n")  # Ajouter des tirets pour séparer les messages
+    console.config(state="disabled")
+    console.see(END)  # Fait défiler automatiquement vers le bas pour afficher le message le plus récent
+
+# Fonction pour quitter proprement
+def on_closing():
+    global sending
+    if connected:
+        if messagebox.askokcancel("Quitter", "Voulez-vous vraiment quitter ?"):
+            if sending:
+                stop_data()  # Arrêter l'envoi de données s'il est en cours
+            disconnect_serial()  # Déconnecter proprement le port série
+            root.destroy()
+    else:
+        if messagebox.askokcancel("Quitter", "Voulez-vous vraiment quitter ?"):
+            root.destroy()
+
+# Charger les données du fichier CSV par défaut
+data = pd.DataFrame()
+
+# Créer une fenêtre
+root = Tk()
+root.title("Pasta Box")
+root.geometry("600x400")
+root.iconbitmap('mipi.ico')
+# Combobox pour sélectionner le port COM
+port_combo = ttk.Combobox(root, width=30)
+port_combo.place(x=10,y=10)
+
+# Bouton pour refresh les ports COM disponibles
+refresh_button = Button(root, text="Refresh Ports",relief='flat',bg="white", command=refresh_ports)
+refresh_button.place(x=310,y=8)
+refresh_ports()  # Rafraîchir la liste des ports dès le démarrage
+
+# Bouton pour charger un fichier CSV
+load_csv_button = Button(root, text="Load CSV",relief='flat',bg="white", command=load_csv)
+load_csv_button.place(x=10,y=50)
+
+# Bouton pour se connecter ou se déconnecter
+connect_button = Button(root, text="Connect",relief='flat',bg="white", command=connect_serial)
+connect_button.place(x=230,y=8)
+
+# Bouton pour démarrer ou arrêter l'envoi de données
+send_button = Button(root, text="Start Sequence", command=toggle_send_data, state="disabled", bg="gray", fg="white",relief='flat')
+send_button.place(y=10, x=500)
+
+# Console pour afficher les messages
+console = Text(root, height=17,width=72, state="disabled", wrap="word")
+console.place(x=10 , y=100)
+
+# Fonction pour quitter proprement
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
